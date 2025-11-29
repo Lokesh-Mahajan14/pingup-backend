abb633182b73b33b8eec2d9f0497b4fe59acc68f
Lokesh-Mahajan14 <lmahajan016@gmail.com>
2025-11-29 17:13:53 +0530

    Initial backend commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDb=async()=>{
+
+    try{
+        mongoose.connection.on('connected',()=>console.log('Database connected'))
+        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
+
+    }catch(error){
+        console.log(error)
+
+    }
+
+}
+export default connectDb;
